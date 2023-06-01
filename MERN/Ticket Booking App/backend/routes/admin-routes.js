5abbfddebf9f334cedfe02970ab8c9c39fc802c5
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-01 22:05:02 +0530

    Update

--- MERN/Ticket Booking App/backend/routes/admin-routes.js
+++ MERN/Ticket Booking App/backend/routes/admin-routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { getAdminById, getAdmins } from '../controllers/admin-controllers.js';
 
 let adminRouter = express.Router();
 
 adminRouter.post('/signup');
 adminRouter.post('/login');
-adminRouter.get('/');
-adminRouter.get('/:id');
+adminRouter.get('/', getAdmins);
+adminRouter.get('/:id', getAdminById);
 
 export default adminRouter;
--- MERN/Ticket Booking App/backend/routes/user-routes.js
+++ MERN/Ticket Booking App/backend/routes/user-routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { getUserById, getUsers } from '../controllers/user-controllers.js';
 
 let userRouter = express.Router();
 
-userRouter.get('/');
-userRouter.get('/:id');
+userRouter.get('/', getUsers);
+userRouter.get('/:id', getUserById);
 userRouter.post('/signup');
 userRouter.post('/login');
 
