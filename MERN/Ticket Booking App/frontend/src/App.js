033d6a4bb1016fa1e6422be08c2e659d6bb8bf01
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-04 15:00:26 +0530

    Update

--- MERN/Ticket Booking App/frontend/src/App.js
+++ MERN/Ticket Booking App/frontend/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Forms from './components/Forms/Forms';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/login" Component={Forms} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ MERN/Ticket Booking App/frontend/src/components/Forms/Forms.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Forms = () => {
+  return <div>Hello from form</div>;
+};
+
+export default Forms;
