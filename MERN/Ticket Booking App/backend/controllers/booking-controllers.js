b51c87da0d991686bddf2ad7c430a6d2adb287b5
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-13 21:00:25 +0530

    Update

--- MERN/Ticket Booking App/backend/controllers/booking-controllers.js
+++ MERN/Ticket Booking App/backend/controllers/booking-controllers.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import Booking from '../models/Booking.js';
+import Movie from '../models/Movie.js';
+import User from '../models/User.js';
 
 export const getBookingById = async (req, res) => {
   const { id } = req.params;
@@ -17,7 +19,49 @@ export const getBookingById = async (req, res) => {
   return res.status(200).json({ booking });
 };
 
-export const newBooking = async (req, res) => {};
+export const newBooking = async (req, res) => {
+  const { movie, date, seatNumber, user } = req.body;
+  let existingUser, existingMovie;
+
+  try {
+    existingMovie = await Movie.findById(movie);
+    existingUser = await User.findById(user);
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!existingMovie || !existingUser) {
+    return res.status(404).json({ message: 'Invalid Details' });
+  }
+
+  let booking;
+
+  try {
+    booking = new Booking({
+      movie,
+      date: new Date(`${date}`),
+      seatNumber,
+      user,
+    });
+
+    const session = mongoose.startSession();
+    session.startSession();
+    existingMovie.bookings.push(booking);
+    existingUser.allBookings.push(booking);
+    await existingMovie.save({ session });
+    await existingUser.save({ session });
+    await booking.save({ session });
+    await session.commitTransaction();
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!booking) {
+    return res.status(500).json({ message: 'Something went Wrong' });
+  }
+
+  return res.status(201).json({ message: 'Booking Successfull' });
+};
 
 export const deleteBooking = async (req, res) => {
   const { id } = req.body;
--- MERN/Ticket Booking App/backend/routes/booking-routes.js
+++ MERN/Ticket Booking App/backend/routes/booking-routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import {
+  deleteBooking,
+  getBookingById,
+  newBooking,
+} from '../controllers/booking-controllers.js';
 
 const bookingRoutes = express.Router();
 
-bookingRoutes.get('/:id');
-bookingRoutes.post('/book');
-bookingRoutes.delete('/:id');
+bookingRoutes.get('/:id', getBookingById);
+bookingRoutes.post('/book', newBooking);
+bookingRoutes.delete('/:id', deleteBooking);
 
 export default bookingRoutes;
