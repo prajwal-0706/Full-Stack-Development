4afab9ade5eeeccb90cb22ffcb42b96f7094ccaf
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-17 00:50:41 +0530

    Update

--- MERN/Ticket Booking App/frontend/src/App.js
+++ MERN/Ticket Booking App/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Forms from './components/Forms/Forms';
 import NavBar from './components/Navbar/Navbar';
 
 function App() {
@@ -10,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" Component={NavBar} />
-          <Route path="/admin" Component={Forms} />
+          {/* <Route path="/admin" Component={Forms} /> */}
         </Routes>
       </Router>
     </div>
--- MERN/Ticket Booking App/frontend/src/components/NavBar/NavBar.jsx
+++ MERN/Ticket Booking App/frontend/src/components/NavBar/NavBar.jsx
@@ -1,19 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import { Button, Typography, Modal, Box } from '@mui/material';
 
 const NavBar = () => {
+  const [open, setopen] = useState(false);
+
+  const styles = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    textAlign: 'center',
+  };
+
   return (
-    <div className="navbar">
-      <div className="navbar__left">
-        <Link to="/products" className="link">
-          <span className="logo">Ticket Booking App</span>
-        </Link>
-        <Link to="/admin" className="link">
-          <span className="logo">Admin SignUp</span>
-        </Link>
-      </div>
-    </div>
+    <>
+      <Button
+        sx={{
+          m: 5,
+          textAlign: 'center',
+          outline: 'none',
+          fontWeight: '500',
+        }}
+        variant="outlined"
+        onClick={() => setopen(true)}
+      >
+        Sign In
+      </Button>
+      <Modal
+        open={open}
+        onClose={() => setopen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={styles}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h1"
+            sx={{
+              mb: 2,
+            }}
+          >
+            Login
+          </Typography>
+          <TextField
+            sx={{
+              mb: 2,
+            }}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <TextField
+            sx={{
+              mb: 2,
+            }}
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
+          <Button variant="contained">Login</Button>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
