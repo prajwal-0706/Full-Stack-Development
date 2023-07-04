6e3bfb1456ca3f75c49b3942ef43a04e5f2473fd
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-07-05 03:02:54 +0530

    Update

--- MERN/Ticket Booking App/frontend/user/src/App.js
+++ MERN/Ticket Booking App/frontend/user/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/Navbar/Navbar';
+import SwiperGallery from './components/Swiper/Swiper';
+import MovieList from './components/Movie/MovieList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <SwiperGallery />
+      <MovieList />
+      <Router>
+        <Routes>{/* <Route path="/" Component={NavBar} /> */}</Routes>
+      </Router>
     </div>
   );
 }
